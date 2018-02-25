507c736869dcbd27826b2ed463388f33f98a0b6c
Netanel86 <iting86@gmail.com>
2018-02-25 05:35:37 +0200

    removed parcelable, redundent when using json

--- app/src/main/java/com/freelance/netanel/androidsearchapp/model/Product.java
+++ app/src/main/java/com/freelance/netanel/androidsearchapp/model/Product.java
@@ -7,15 +7,13 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * Represents a product.
- * Implements Parcelable, for passing instances when using Intent.
  * @see Parcelable
  * @see android.content.Intent
  * @author Netanel Iting
  * @version %I%, %G%
  * @since 1.0
  */
-public class Product implements Parcelable {
-    @SerializedName("id")
+public class Product{
     private int id;
     public int getId() {
         return id;
@@ -24,7 +22,6 @@ public class Product implements Parcelable {
         this.id = id;
     }
 
-    @SerializedName("name")
     private String name;
     public String getName() {
         return name;
@@ -33,7 +30,6 @@ public class Product implements Parcelable {
         this.name = name;
     }
 
-    @SerializedName("imageUrl")
     private String imageUrl;
     public String getImageUrl() {
         return imageUrl;
@@ -42,7 +38,6 @@ public class Product implements Parcelable {
         this.imageUrl = imageUrl;
     }
 
-    @SerializedName("description")
     private String description;
     public String getDescription() {
         return description;
@@ -57,65 +52,4 @@ public class Product implements Parcelable {
         this.description = description;
         this.imageUrl = imageUrl;
     }
-
-    ///region Parcelable
-    public static final Parcelable.Creator<Product> CREATOR = new Parcelable.Creator<Product>() {
-        @Override
-        public Product createFromParcel(Parcel source) {
-            return new Product(source);
-        }
-
-        @Override
-        public Product[] newArray(int size) {
-            return new Product[size];
-        }
-    };
-
-    private Product(Parcel inputParcel) {
-        id = inputParcel.readInt();
-        name = inputParcel.readString();
-        description = inputParcel.readString();
-        imageUrl = inputParcel.readString();
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeInt(id);
-        dest.writeString(name);
-        dest.writeString(description);
-        dest.writeString(imageUrl);
-    }
-    ///endregion
-
-    ///region Equals
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Product product = (Product) o;
-
-        if (id != product.id) return false;
-        if (name != null ? !name.equals(product.name) : product.name != null) return false;
-        if (imageUrl != null ? !imageUrl.equals(product.imageUrl) : product.imageUrl != null) {
-            return false;
-        }
-        return description != null ? description.equals(product.description)
-                : product.description == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        result = 31 * result + (imageUrl != null ? imageUrl.hashCode() : 0);
-        result = 31 * result + (description != null ? description.hashCode() : 0);
-        return result;
-    }
-    ///endregion
 }
